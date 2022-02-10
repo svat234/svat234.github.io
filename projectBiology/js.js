43ca5e1060b66c624e6b89d24910cf4c8a979596
svat234 <80785392+svat234@users.noreply.github.com>
2022-02-10 12:30:21 +0200

    Update js.js

--- projectBiology/js.js
+++ projectBiology/js.js
@@ -1,15 +1,17 @@
 function Animation(){
-    var base = document.getElementById('base')
-    base.style.transform = 'rotateY('+String(Number(base.style.transform.slice(8, -4))+360)+'deg)'
+    var base = document.getElementById('base');
+    var pagecounter = document.getElementById('page-counter');
+    base.style.transform = 'rotateY('+String(Number(base.style.transform.slice(8, -4))+180)+'deg)';
+    pagecounter.style.transform = 'rotateY('+String(Number(base.style.transform.slice(8, -4))*-1)+'deg)'
 }
 function ChangePage(pageId){
-    var content = document.getElementById('content')
+    var content = document.getElementById('content');
     Animation();
     setTimeout(() => {
-        content.getElementsByClassName('page active')[0].className = 'page';
-        document.getElementById(pageId).className = 'page active'
-    }, 500);
+        content.getElementsByClassName('page active')[0].classList.remove('active');
+        document.getElementById(pageId).classList.add('active');
+    }, 250);
 }
 function ActivateButton(buttonId){
-    document.getElementById(buttonId).className = 'button';
+    document.getElementById(buttonId).classList.remove('disabled');
 }

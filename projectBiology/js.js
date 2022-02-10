23cb2bac3c479ccee9a9f6de6bc1e8486fb91556
svat234 <80785392+svat234@users.noreply.github.com>
2022-02-10 12:49:22 +0200

    Update js.js

--- projectBiology/js.js
+++ projectBiology/js.js
@@ -12,6 +12,12 @@ function ChangePage(pageId){
         document.getElementById(pageId).classList.add('active');
     }, 250);
 }
+function PageCounterUpdate(){
+    var pagecounter = document.getElementById('page-counter');
+    var id = pagecounter.getElementsByClassName('selected')[0].id
+    document.getElementById(id).classList.remove('selected');
+    document.getElementById('page-counter-element-'+String(Number(id.slice(21))+1)).classList.add('selected')
+}
 function ActivateButton(buttonId){
     document.getElementById(buttonId).classList.remove('disabled');
 }
